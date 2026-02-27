d7fc4cd4f472ce5fcbac5f23d35f275d2ec2be46
Santosh Singh <santosh102969@gmail.com>
2026-02-27 23:49:39 +0530

    Adds initial dependency and setup configuration
    Introduces project dependencies and basic package setup
    to enable environment reproducibility and facilitate future
    development and deployment workflows.

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+with open("README.md" , 'r',encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "End-to-End-Kideny-Disease-classification-Project"
+AUTHOR_NAME = "Santosh Singh"
+SRC_REPO = "cnnClassifier"
+AUTHOR_EMAIL = "santosh102969@gmail.com"
+
+setuptools.setup(
+    name = SRC_REPO,
+    
+)
